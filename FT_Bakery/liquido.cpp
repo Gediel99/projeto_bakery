0510300e67364b046575f4335b8625908db5caa9
Gediel Durães de Almeida <95309923+Gediel99@users.noreply.github.com>
2023-06-06 17:02:37 -0300

    Adiciona o arquivo liquido (cpp e hpp) e cria a classe Liquid() dentro dele

---
+++ FT_Bakery/liquido.cpp
@@ -0,0 +1,16 @@
+#include <string>
+#include "comida.hpp"
+#include "liquido.hpp"
+
+using namespace std;
+
+Liquid::Liquid(int unidades, double valor) : Food(valor)
+   {
+   this->unidades = unidades;
+   };
+   
+string Liquid::getDescricao()
+   { 
+   return (to_string(unidades) + " un/pacote."); 
+   };
+   
